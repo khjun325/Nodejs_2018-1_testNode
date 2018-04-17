340a504a4d69f2f38b67a3e8e5654e6906d05c06
paul <khjun325@naver.com>
2018-04-17 10:41:13 +0900

    [20180417 first test]

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-console.log(typeof console === 'function');
+/*console.log(typeof console === 'function');
 console.log('Node.js version : ' + process.version);
 console.log(process.memoryUsage());
-console.log(process.env);
+console.log(process.env);*/
+//console.log(module);
+
+const circle = require('./circle');
+console.log('Area of ac circle =' + circle.area(3)); //모듈을 사용한것
+console.log('Circumference of ac circle =' + circle.circum(3));
+console.log('Pi =' + circle.pi);

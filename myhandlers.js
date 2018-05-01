06e2bc948418303709b78542e20e709cc1fab54b
paul <khjun325@naver.com>
2018-05-01 11:23:31 +0900

    [20180417 first test]

--- myhandlers.js
+++ myhandlers.js
@@ -1,6 +1,6 @@
 function sleep(millisec) {
-    let timeStart = new Data().getTime();
-    while (new Data().getTime() < timeStart + millisec); //new를 써야만 생성자 호출되어 필요한 초기화가 가능
+    let timeStart = new Date().getTime();
+    while (new Date().getTime() < timeStart + millisec); //new를 써야만 생성자 호출되어 필요한 초기화가 가능
 }
 
 function start(res){
@@ -9,10 +9,12 @@ function start(res){
     res.end();
 }
 function hello(res){
-    sleep(20000); //20초
+    //sleep(20000); //20초
+    setTimeout(function () {
     res.writeHead(200, {'Content-Type' : 'text/html'});
     res.write('hi! my name is hyung');
     res.end();
+},20000);
 }
 exports.start =start;
 exports.hello = hello;

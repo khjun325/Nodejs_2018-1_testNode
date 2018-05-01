1940f00a9d4d2317921117aa9e049d3182f08972
paul <khjun325@naver.com>
2018-05-01 11:08:36 +0900

    [20180417 first test]

--- myServer.js
+++ myServer.js
@@ -4,12 +4,9 @@ const url = require('url');
 function start (port,hostname, route, handle){
     function onRequest(req,res){
         let sPathname=url.parse(req.url).pathname;
-        let content = route(sPathname,handle);
-        res.writeHead(200,{'Content-type':'text/html'});
-        res.write(content);
-        res.end();
+        route(sPathname, handle, res);
     }
-    http.createServer(onRequest).listen(port,hostname);
+    http.createServer(onRequest).listen(port,hostname);  //콜백 요청을 보낼때 onRequest가 뒤에 호출됨
     console.log('Server is running at '+ hostname + ':' + port);
 }
 
--- myhandlers.js
+++ myhandlers.js
@@ -1,8 +1,12 @@
-function start(){
-    return 'Hello, start!';
+function start(res){
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write('hi! my name is kim');
+    res.end();
 }
-function hello(){
-    return 'Hello, hello!';
+function hello(res){
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write('hi! my name is hyung');
+    res.end();
 }
 exports.start =start;
 exports.hello = hello;

82946c0b64561a988c29823201b893f654b434c8
paul <khjun325@naver.com>
2018-05-01 12:54:06 +0900

    [20180417 first test]

--- myhandlers.js
+++ myhandlers.js
@@ -1,4 +1,10 @@
 const queryString = require('querystring')
+const twitter = require('./brainDemoTwitterDonKim');
+const myBrain = require('./myBrain');
+const myData = require('./twitterDonKim');
+
+let trainedNet = myBrain.train(myData.trainingData);
+
 function sleep(millisec) {
     let timeStart = new Date().getTime();
     while (new Date().getTime() < timeStart + millisec); //new를 써야만 생성자 호출되어 필요한 초기화가 가능
@@ -11,7 +17,6 @@ function start(res) {
         '이름과 별명을 입력하세요.<br>' +
         '<form action="/hello" method="post">' +
         '<input type="text" name="myName" /><br>' +
-        '<input type="text" name="myNick" /><br>' +
         '<button type="submit">입력 완료</button>' +
         '</form>' + '</body>' + '</html>';
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -19,11 +24,14 @@ function start(res) {
     res.end();
 }
 function hello(res, postData) {
+    let input = queryString.parse(postData).myName;
+    let result = myBrain.getResult(trainedNet,input);
+
     let sBody = '<html>' + '<head>' +
         '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />' +
         '</head>' + '<body>' +
-        '안녕하세요, ' + queryString.parse(postData).myName +
-        '(별명: ' + queryString.parse(postData).myNick + ')님!' +
+        'Trump, ' + Math.round(result.trump*100) + 'and'+
+        'kardashian ' + Math.round(result.kardashian*100) + 
         '</body>' + '</html>';
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(sBody);
